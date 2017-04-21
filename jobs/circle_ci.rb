dfbc087a122c9f3029e46a708d67eabaf708e0e0
Nick Redshaw <nickredshaw@gmail.com>
2017-04-21 16:36:15 +0100

    CSRA-330: Add health endpoint polling to the same dashboard as the CircleCI status and poll the MOCK and STAGE environments

--- jobs/circle_ci.rb
+++ jobs/circle_ci.rb
@@ -1,6 +1,9 @@
 require 'httparty'
 # require 'digest/md5'
 
+httptimeout = 60
+ping_count = 10
+
 projects = [
   { user: 'noms-digital-studio', repo: 'csra-mvp', branch: 'master'},
   { user: 'noms-digital-studio', repo: 'csra-app', branch: 'master'},
@@ -62,10 +65,63 @@ def build_data(project, auth_token)
   return data
 end
 
+servers = [
+    {name: 'csra-app-mock', url: 'http://csra-mock.hmpps.dsd.io/health', method: 'http'},
+    {name: 'csra-app-stage', url: 'http://csra-stage.hmpps.dsd.io/health', method: 'http'},
+]
+
 SCHEDULER.every '10s', :first_in => 0  do
   projects.each do |project|
     data_id = "circle-ci-#{project[:user]}-#{project[:repo]}-#{project[:branch]}"
     data = build_data(project, ENV['CIRCLE_CI_TOKEN'])
     send_event(data_id, data) unless data.empty?
   end
+
+  servers.each do |server|
+    if server[:method] == 'http'
+      begin
+        uri = URI.parse(server[:url])
+
+        http = Net::HTTP.new(uri.host, uri.port)
+        http.read_timeout = httptimeout
+        if uri.scheme == "https"
+          http.use_ssl=true
+          http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+        end
+
+        request = Net::HTTP::Get.new(uri.request_uri)
+        if server[:auth] == true
+          request.basic_auth basic_auth.split(':').first, basic_auth.split(':').last
+        end
+
+        response = http.request(request)
+
+        if response.code == "200"
+          result = 1
+        else
+          result = 0
+        end
+      rescue Timeout::Error
+        result = 0
+      rescue Errno::ETIMEDOUT
+        result = 0
+      rescue Errno::EHOSTUNREACH
+        result = 0
+      rescue Errno::ECONNREFUSED
+        result = 0
+      rescue SocketError => e
+        result = 0
+      end
+    elsif server[:method] == 'ping'
+      result = `ping -q -c #{ping_count} #{server[:url]}`
+      if ($?.exitstatus == 0)
+        result = 1
+      else
+        result = 0
+      end
+    end
+
+    send_event(server[:name], result: result)
+  end
+
 end
